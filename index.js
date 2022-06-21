e7aa35d229b05198413e22f08dcce7ddf0706987
Adheesh <95172854+Adheesh-Sood@users.noreply.github.com>
2022-06-21 07:57:36 +0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+document.getElementById("goVid").onclick = function() {
+    window.location.href = "videos.html"
+}
+
+document.getElementById("goLearn").onclick = function() {
+    window.location.href = "learn.html"
+}
+
+document.getElementById("goShare").onclick = function() {
+    window.location.href = "share.html"
+}
+document.getElementById("goView").onclick = function() {
+    document.location.href = "view.html"
+}
--- view.js
+++ view.js
@@ -1,12 +1,81 @@
 //JS 
+//Variables 
+var lang = "none";
+//................................................................
 document.getElementById('py').style.display = "none"
+document.getElementById("innerSearch").style.display = "none"
     //................................................................
 document.getElementById("go").onclick = function() {
     var search = document.getElementById("searchLang").value;
     if (search == "python") {
+        document.getElementById("innerSearch").value = "Python :: "
+        document.getElementById("innerBtn").style.display = ""
+        lang = "python";
         document.getElementById("py").style.display = ""
     } else if (search == "Python") {
+        lang = "python";
+        document.getElementById("innerSearch").style.display = ""
+        document.getElementById("innerSearch").value = "Python :: "
+
         document.getElementById("py").style.display = ""
 
+    } else if (search == "HTML") {
+        document.getElementById("innerSearch").value = "HTML :: "
+        document.getElementById("innerBtn").style.display = ""
+        lang = "html";
+        window.alert("Html")
+
+    } else if (search == "html") {
+        document.getElementById("innerSearch").value = "HTML :: "
+
+        document.getElementById("innerBtn").style.display = ""
+        lang = "html";
+        window.alert("HTML")
+    } else if (search == "CSS") {
+        document.getElementById("innerSearch").value = "CSS :: "
+
+        document.getElementById("innerBtn").style.display = ""
+        lang = "css";
+        Window.alert("CSS")
+    } else if (search == "css") {
+        document.getElementById("innerSearch").value = "CSS :: "
+
+        document.getElementById("innerBtn").style.display = ""
+        lang = "css";
+        Window.alert("CSS")
+    } else if (search == "javascript") {
+        document.getElementById("innerSearch").value = "Javascript :: "
+
+        document.getElementById("innerBtn").style.display = ""
+        lang = "js"
+        window.alert("Javascript")
+    } else if (search == "Javascript") {
+        document.getElementById("innerSearch").value = "CSS :: "
+
+        document.getElementById("innerBtn").style.display = ""
+        lang = "js"
+        window.alert("Javascript")
+    } else if (search == "C++") {
+        document.getElementById("innerSearch").value = "C++ :: "
+
+        document.getElementById("innerBtn").style.display = ""
+        lang = "c++";
+        window.alert("C++")
+    } else if (search == "C#") {
+        document.getElementById("innerSearch").value = "C# :: "
+
+        document.getElementById("innerBtn").style.display = ""
+        lang = "c#";
+        window.alert("C#")
+    } else {
+        lang = "none";
+        window.alert("No language found as" + search)
     }
+
+
+
+
+}
+while (lang != "none") {
+    document.getElementById("innerBtn").style.display = ""
 }
